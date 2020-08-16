c1a7b247c92be7f5e6b8596d8523c9dc4de57094
marcovigano <marco.vigano@ymail.com>
2020-08-16 18:10:37 +0100

    working out test of sentiment Handler

--- src/client/js/formHandler.test.js
+++ src/client/js/formHandler.test.js
@@ -1,39 +1,47 @@
-// import "@babel/polyfill"
-//
-// const sut = require('./formHandler');
-//
-// const sentimentHandler = require('./sentimentHandler');
-//
-// jest.mock('./sentimentHandler');
-//
-// it('should display the weather info', async () => {
-//
-//
-//     document.body.innerHTML = `
-//         <div class="output">
-//             <div id="polarity"></div>
-//             <div id="subjectivity"></div>
-//             <div id="polarity_confidence"></div>
-//             <div id="subjectivity_confidence"></div>
-//         </div>
-//   `;
-//
-//     sentimentHandler.getSentiment.mockResolvedValue({
-//         data:
-//             {
-//                 polarity:"positive",
-//                 subjectivity:"subjective",
-//                 text:"Lebron is the king",
-//                 polarity_confidence:0.78,
-//                 subjectivity_confidence:0.45
-//             }
-//     });
-//
-//     event.preventDefault();
-//
-//     await sut.handleSubmit();
-//
-//     expect(document.querySelector('#polarity').innerHTML).toEqual('positive');
-//
-// });
-//
+import "@babel/polyfill"
+
+const sut = require('./formHandler');
+
+const sentimentHandler = require('./sentimentHandler');
+import Client from './sentimentHandler'
+
+it('should display the weather info', async () => {
+
+
+    document.body.innerHTML = `
+        <div class="input" >
+            <form class="input-area">
+                <textarea id="input-text" placeholder="Write something here..."></textarea>
+                <input id="submit-botton" type="submit" value="submit" onclick="return Client.handleSubmit(event)" onsubmit="return Client.handleSubmit(event)">
+            </form>
+        </div>
+        <div class="output">
+            <div id="polarity"></div>
+            <div id="subjectivity"></div>
+            <div id="polarity_confidence"></div>
+            <div id="subjectivity_confidence"></div>
+        </div>
+  `;
+    jest.mock('./sentimentHandler');
+
+    sentimentHandler.getSentiment = jest.fn().mockResolvedValue({
+        data:
+            {
+                polarity:"positive",
+                subjectivity:"subjective",
+                text:"Lebron is the king",
+                polarity_confidence:0.78,
+                subjectivity_confidence:0.45
+            }
+    });
+
+    const myMockElement = {
+        preventDefault: jest.fn(),
+    };
+
+    await sut.handleSubmit(myMockElement);
+
+    expect(document.querySelector('#polarity').innerHTML).toEqual('positive');
+
+});
+
